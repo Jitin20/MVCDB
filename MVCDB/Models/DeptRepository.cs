70644306988cbb7e9f094292c52c8f8c7a4bc363
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-16 17:36:55 +0530

    commit

---
+++ MVCDB/Models/DeptRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCDB.Models
+{
+    public class DeptRepository : IDept
+    {
+        public void AddDept(Dept dept)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDept(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditDept(Dept dept)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dept FindDept(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dept> GetDepts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
