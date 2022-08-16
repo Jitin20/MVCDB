eea09037fae2c681457b1e669f32093e976439b1
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-16 17:33:10 +0530

    Add project files.

---
+++ MVCDB/Models/IDept.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MVCDB.Models
+{
+    public interface IDept
+    {
+        List<Dept> GetDepts();
+        Dept FindDept(int id);
+        void AddDept(Dept dept);
+        void EditDept(Dept dept);
+        void DeleteDept(int id);        
+
+    }
+}
