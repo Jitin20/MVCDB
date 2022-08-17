72eec8044c00c17dc4da589f51b22435ff3c1ce1
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-17 18:09:48 +0530

    Added MainLayout and ViewStart

--- MVCDB/Controllers/DeptController.cs
+++ MVCDB/Controllers/DeptController.cs
@@ -24,7 +24,7 @@ namespace MVCDB.Controllers
             var data = repos.FindDept(id);
             return View(data);
         }
-
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -32,13 +32,14 @@ namespace MVCDB.Controllers
         [HttpPost]
         public IActionResult Create(Dept dept)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 repos.AddDept(dept);
                 return RedirectToAction("List");
             }
             return View(dept);
         }
+        [HttpGet]
         public IActionResult Edit(int id)
         {
             var data = repos.FindDept(id);
@@ -47,12 +48,25 @@ namespace MVCDB.Controllers
         [HttpPost]
         public IActionResult Edit(Dept dept)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 repos.EditDept(dept);
                 return RedirectToAction("List");
             }
             return View(dept);
+        }
+        [HttpGet]
+        public IActionResult Deleted(int id)
+        {
+            var data = repos.FindDept(id);
+                return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Deleted(Dept dept)
+        {
+            repos.DeleteDept(dept.Id);
+            return RedirectToAction("List");
         }
     }
 }
