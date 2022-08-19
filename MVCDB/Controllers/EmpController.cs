8fe41e6462133ce8a1652cbd086fae7ca6e9fd23
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-19 11:19:36 +0530

    Commit emp controller and views

---
+++ MVCDB/Controllers/EmpController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDB.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVCDB.Controllers
+{
+
+  
+    public class EmpController : Controller
+    {
+       db1045Context db= new db1045Context();
+        //for fetching specific columns from emp and also dept name here is what we do
+        public IActionResult List()
+        {
+            var empdata = db.Emps.Include("Dept").ToList();
+            return View(empdata);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.DeptId = new SelectList(db.Depts, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Emp emp)
+        {
+            //We have to create privison for drop down
+         
+            if (ModelState.IsValid)
+            {
+                db.Emps.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.DeptId = new SelectList(db.Depts, "Id", "Name");
+            return View(emp);
+        }
+        
+    }
+}
--- MVCDB/Models/Emp.cs
+++ MVCDB/Models/Emp.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
-#nullable disable
-
+using System.ComponentModel.DataAnnotations;
 namespace MVCDB.Models
 {
     public partial class Emp
     {
+        [Display(Name = "Emp Code")]
         public int Id { get; set; }
+        [Display(Name = "Emp Name")]
+        [Required(ErrorMessage = "Name can not be Blank")]
         public string Name { get; set; }
+        [Display(Name = "Emp Salary")]
+        [Range(10000, 90000, ErrorMessage = "Salary should be in the range")]
         public int? Salary { get; set; }
+        [Display(Name = "Department ID")]
         public int? Deptid { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd)")]
         public DateTime? Dob { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         public virtual Dept Dept { get; set; }
